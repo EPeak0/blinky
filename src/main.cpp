2d3f5950a84ea468463a78324aea27c2c88d2bd2
EPeak0 <die.more@outlook.com>
2025-09-17 11:39:31 +0200

    Update prj.conf and main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,31 @@
+// stl
+#include <chrono>
+
+// zpp-lib
+#include "zpp_include/thread.hpp"
+#include "zpp_include/this_thread.hpp"
+#include "zpp_include/digital_out.hpp"
+
+void blink() {
+    zpp_lib::DigitalOut led(zpp_lib::DigitalOut::PinName::LED0);
+    using namespace std::literals;
+    static std::chrono::milliseconds blinkInterval = 1000ms;
+
+    while (true) {
+        led = !led;
+        zpp_lib::ThisThread::sleep_for(blinkInterval);
+    }
+}
+
 int main(void) {
-  while (true) {}
-  return 0;
+    zpp_lib::Thread thread;
+    auto res = thread.start(blink);
+    if (! res) {
+        return -1;
+    }
+    res = thread.join();
+    if (! res) {
+        return -1;
+    }
+    return 0;
 }
