a5a851214e0a46d79da2aebff26d9f6baf6b1f37
EPeak0 <die.more@outlook.com>
2025-09-18 21:12:53 +0200

    add debug capabillity

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include "zpp_include/thread.hpp"
 #include "zpp_include/this_thread.hpp"
 #include "zpp_include/digital_out.hpp"
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(blinky, CONFIG_APP_LOG_LEVEL);
 
 void blink() {
     zpp_lib::DigitalOut led(zpp_lib::DigitalOut::PinName::LED0);
@@ -18,14 +21,20 @@ void blink() {
 }
 
 int main(void) {
+    LOG_DBG("Running on board %s", CONFIG_BOARD_TARGET);
+    LOG_DBG("Starting thread");
     zpp_lib::Thread thread;
     auto res = thread.start(blink);
     if (! res) {
+        LOG_ERR("Could not start thread (%d)", static_cast<int>(res.error()));
         return -1;
     }
+
     res = thread.join();
     if (! res) {
+        LOG_ERR("Could not terminate thread (%d)", static_cast<int>(res.error()));
         return -1;
     }
+
     return 0;
 }
